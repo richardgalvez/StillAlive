e6f6d8a6f138d85e4a544192978c4477005de58d
Richard Galvez <richardalbertogalvez@gmail.com>
2026-01-27 22:39:56 -0500

    Handles non-existent sites and list of sites.

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-# Send HTTP request to site or list of sites
-
 import requests
 
-name = "https://richardgalvez.com"
-http_request = requests.get(name).status_code
+sites = ["https://richardgalvez.com",
+         "https://google.com/dsacxzcqqc",
+         "https://zgopggle12.org",
+         "https://chatgpt.com/freegptforlife"]
 
-def check_site(sitename: str, status_code: int):
+def check_site(sitename: str):
+    try:
+        status_code = requests.get(sitename).status_code
+    except requests.exceptions.ConnectionError:
+        return f"{sitename} - Connection Failed - Site Name Incorrect or Does Not Exist."
+
     if status_code == 200:
-        return f"{sitename} - Site is Up!"
+        return f"{sitename} - {status_code} - Site is Up"
     else:
-        return f"{sitename} - Site Not Accessible..."
+        return f"{sitename} - {status_code} - Site Not Accessible"
 
-print(check_site(name, http_request))
+for site in sites:
+    print(check_site(site))
