f823173828b84490ee71d77f876152ae619e13f8
Richard Galvez <richardalbertogalvez@gmail.com>
2026-01-28 19:54:22 -0500

    Added more sites for testing.

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import requests
 
 sites = ["https://richardgalvez.com",
          "https://google.com/dsacxzcqqc",
+         "https://amazon.com",
          "https://zgopggle12.org",
-         "https://chatgpt.com/freegptforlife"]
+         "https://linkedin.com",
+         "https://chatgpt.com/freegptforlife",
+         "https://youtube.com",
+         "https://github.com",
+         "https://robotoverlords.gov"]
 
 def check_site(sitename: str):
     try:
